30c0f2f2870ae2145abd51531954d494f507c87e
andreas <andreas.baron@rubikon.news>
2022-10-04 19:32:03 +0200

    update for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,6 +96,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'https://freie-medienakademie.de' }
+  config.action_mailer.default_url_options = { host: 'freie-medienakademie.de' }
 
 end
