4c4a125e170959439aaf04e7a2e005e6a012f9ab
andreas <andreas.baron@rubikon.news>
2022-10-06 21:15:13 +0200

    update articles show for index and medienplus

--- app/controllers/medienplus_controller.rb
+++ app/controllers/medienplus_controller.rb
@@ -1,6 +1,6 @@
 class MedienplusController < FrontendController
 	def index
-		@published_articles = Article.where(publish: true).all		
+		@published_articles = Article.where(publish: true).all.order(date: :desc)				
 	end
 
 	def show
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < FrontendController
 
 	def index
-		
+		@published_articles = Article.where(publish: true).limit(3).order(date: :desc)		
 	end
 end
