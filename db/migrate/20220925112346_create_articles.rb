78600272e7e4760ebcfc650c4d0ab50858abbfd3
andreas <andreas.baron@rubikon.news>
2022-10-13 16:43:15 +0200

    updated database fields for articles

--- db/migrate/20220925112346_create_articles.rb
+++ db/migrate/20220925112346_create_articles.rb
@@ -3,16 +3,16 @@ class CreateArticles < ActiveRecord::Migration[5.1]
     create_table :articles do |t|
       t.datetime  :date      
       t.string    :title     
-      t.string    :intro     
+      t.text      :intro, limit: 16380     
       t.string    :author    
       t.string    :category  
       t.string    :sources
-      t.text    :image_data
+      t.text      :image_data
       t.string    :image_sources
       t.string    :video
       t.string    :teaser
       t.boolean   :publish
-      t.text      :keywords, limit: 16380
+      t.text      :keywords, limit: 32380
       t.text      :text, limit: 16380
     end
   end
