46815adb2ba8de4ee94fbb95eedbb283e473b848
andreas <andreas.baron@rubikon.news>
2022-10-16 14:29:55 +0200

    update dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,12 @@
 class Admin::DashboardController < Admin::AdminController
   def index
-  
+    @articles = Article.all
+    @articles_published = Article.where(publish: true)
+    @users = User.all
+    @students = User.where(role: 'student')
+    @task_articles = TaskArticle.all
+    @tasks = Task.all
+    @messages = Message.all
+    @forms = Form.all
   end
 end
