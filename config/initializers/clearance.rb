4ab1ba4c9af6375841152ba294f8d78ff9f0bb3b
andreas <andreas.baron@rubikon.news>
2022-10-16 13:03:11 +0200

    removed lambda

--- config/initializers/clearance.rb
+++ config/initializers/clearance.rb
@@ -2,5 +2,4 @@ Clearance.configure do |config|
   config.routes = false
   config.mailer_sender = "reply@example.com"
   config.rotate_csrf_on_sign_in = true
-  config.cookie_expiration = lambda { 2.hour.from_now.cet }
 end
