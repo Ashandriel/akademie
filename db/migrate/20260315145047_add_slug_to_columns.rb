2eea8b30b6cbde661f91c1483eb62f5c59afe273
Ashes <ashes@mac.speedport.ip>
2026-03-15 16:48:08 +0100

    fixed migration

--- db/migrate/20260315145047_add_slug_to_columns.rb
+++ db/migrate/20260315145047_add_slug_to_columns.rb
@@ -1,28 +1,6 @@
-class GenerateSlugsForColumns < ActiveRecord::Migration[5.1]
-  class ColumnMigration < ActiveRecord::Base
-    self.table_name = "columns"
-  end
-
-  def up
-    ColumnMigration.reset_column_information
-
-    ColumnMigration.where(slug: [nil, ""]).find_each do |column|
-      next if column.title.blank?
-
-      base_slug = column.title.parameterize
-      slug = base_slug
-      counter = 2
-
-      while ColumnMigration.where.not(id: column.id).exists?(slug: slug)
-        slug = "#{base_slug}--#{counter}"
-        counter += 1
-      end
-
-      column.update_columns(slug: slug)
-      puts "Generated slug for Column ##{column.id}: #{slug}"
-    end
-  end
-
-  def down
+class AddSlugToColumns < ActiveRecord::Migration[5.1]
+  def change
+    add_column :columns, :slug, :string
+    add_index :columns, :slug, unique: true
   end
 end
