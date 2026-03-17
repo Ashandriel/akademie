f54a02362c511e24fca18ad0ec882576b962272c
Ashes <ashes@MacBook-Air-von-Ashes.local>
2026-03-17 09:50:19 +0100

    removed columns link to overview, altered pagination url and included redirects, removed unnecessary div in index

--- app/controllers/columns_controller.rb
+++ app/controllers/columns_controller.rb
@@ -5,16 +5,33 @@ class ColumnsController < ApplicationController
 
   def show
     @column = Column.friendly.find(params[:id])
+
+    short_path =
+      case @column.slug
+      when "kommentare"
+        kommentare_path
+      when "buch-tresen"
+        buch_tresen_path
+      when "medien-tresen"
+        medien_tresen_path
+      else
+        nil
+      end
+
+    if short_path.present? && request.path != short_path
+      target = request.query_string.present? ? "#{short_path}?#{request.query_string}" : short_path
+      return redirect_to target, status: :moved_permanently
+    end
+
     @articles = @column.articles.page(params[:page]).per(9)
   end
 
   def overview
-    @columns = Column.includes(:articles).all # Lädt alle Kolumnen mit ihren Artikeln
+    @columns = Column.includes(:articles).all
   end
 
   def articles
     @column = Column.friendly.find(params[:id])
-    @articles = @column.articles.order(created_at: :desc) # Hole alle Artikel der Kolumne, sortiert nach Erstellungsdatum
+    @articles = @column.articles.order(created_at: :desc)
   end
-
 end
