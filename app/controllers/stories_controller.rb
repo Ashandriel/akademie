d7e3502d2b6b945d2b8915b1bdf3fc2327151fd1
Ashes <ashes@mac.speedport.ip>
2026-05-28 16:48:21 +0200

    update Lebensgeschichten

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,9 +1,16 @@
 class StoriesController < FrontendController
   layout "application" # falls dein article-Layout @article erwartet
+
   def index
-    @published_articles = Article.friendly
-                                 .where(publish: true, category: 'Lebensgeschichten')
-                                 .order(date: :desc)
-                                 .page(params[:page]).per(9)
+    @published_articles =
+      Article.friendly
+             .where(publish: true, category: 'Lebensgeschichten')
+             .order(date: :desc, id: :desc)
+             .page(params[:page])
+             .per(10)
+
+    articles_for_page = @published_articles.to_a
+    @hero_article = articles_for_page.first
+    @grid_articles = articles_for_page.drop(1)
   end
 end
