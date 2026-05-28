e47a1502425077a4ad5f6df633312bfd7cae0e30
Ashes <ashes@mac.speedport.ip>
2026-05-28 15:00:27 +0200

    update articles controller and view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,20 +5,22 @@ class ArticlesController < FrontendController
     @published_articles =
       Article.friendly
              .where(publish: true, category: nil)
+             .order(date: :desc, id: :desc)
              .page(params[:page])
              .per(10)
   end
 
-def search
-  @q = params[:q].to_s.strip
+  def search
+    @q = params[:q].to_s.strip
 
-  @published_articles =
-    Article.friendly
-           .where(publish: true)     # <-- category: nil raus
-           .text_search(@q)
-           .page(params[:page])
-           .per(10)
-end
+    @published_articles =
+      Article.friendly
+             .where(publish: true)
+             .text_search(@q)
+             .order(date: :desc, id: :desc)
+             .page(params[:page])
+             .per(10)
+  end
 
 
   def show
